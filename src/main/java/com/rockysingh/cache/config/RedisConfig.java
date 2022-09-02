68e6372c548225b26bf8bf7b4aa6eb0cebcc1004
Zhou He Sheng <hszhou@ilegendsoft.com>
2022-09-02 09:56:29 +0800

    test

--- src/main/java/com/rockysingh/cache/config/RedisConfig.java
+++ src/main/java/com/rockysingh/cache/config/RedisConfig.java
@@ -6,36 +6,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisConfig{
+public class RedisConfig {
 
-    @Autowired
-    private JedisConnectionFactory jedisConnFactory;
-
-    @Bean
-    public StringRedisSerializer stringRedisSerializer() {
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        return stringRedisSerializer;
-    }
-
-    @Bean
-    public JacksonJsonRedisSerializer<User> jacksonJsonRedisJsonSerializer() {
-        JacksonJsonRedisSerializer<User> jacksonJsonRedisJsonSerializer = new JacksonJsonRedisSerializer<>(User.class);
-        return jacksonJsonRedisJsonSerializer;
-    }
+  @Autowired
+  private JedisConnectionFactory jedisConnFactory;
 
 
-    @Bean
-    public RedisTemplate<String, User> redisTemplate() {
-        RedisTemplate<String, User> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(jedisConnFactory);
-        redisTemplate.setKeySerializer(stringRedisSerializer());
-        redisTemplate.setValueSerializer(jacksonJsonRedisJsonSerializer());
-        return redisTemplate;
-    }
+  @Bean
+  public RedisTemplate<String, User> redisTemplate() {
+    RedisTemplate<String, User> template = new RedisTemplate<>();
+    template.setConnectionFactory(jedisConnFactory);
+    template.setDefaultSerializer(new StringRedisSerializer());
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setValueSerializer(new JacksonJsonRedisSerializer<>(User.class));
+    template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+    return template;
+  }
 
 
 }
